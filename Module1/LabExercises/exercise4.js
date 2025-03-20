fa516fd7e722c892e5d3797725e3e1f548857c44
DustinKalb <dustinkalb9078@gmail.com>
2025-03-19 19:22:46 -0500

    exercises 1 and 4

---
+++ Module1/LabExercises/exercise4.js
@@ -0,0 +1,26 @@
+function sub(a, b) {
+  return a - b;
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+function mul(a, b) {
+  return a * b;
+}
+
+console.log(sub(2, 1));
+console.log(sum(2, 1));
+console.log(div(2, 1));
+console.log(mul(2, 1));
+
+function myFunction(name1) {
+  return "Hello" + " " + name1;
+}
+
+console.log(myFunction("Dustin"));
