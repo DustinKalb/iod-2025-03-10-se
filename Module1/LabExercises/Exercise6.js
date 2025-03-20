3491f4e8b590fd7fdbef48b501d8ecb707613441
DustinKalb <dustinkalb9078@gmail.com>
2025-03-19 20:07:22 -0500

    exercise6

---
+++ Module1/LabExercises/Exercise6.js
@@ -0,0 +1,15 @@
+let book = {
+  title: "Naruto",
+  description: "The Tale of Naruto Uzumaki",
+  author: "Jiraiya",
+  numberofpages: "100",
+};
+
+console.log(book.title);
+console.log(book.description);
+console.log(book.author);
+console.log(book.numberofpages);
+console.log();
+console.log(book);
+
+book.description = "How Naruto Uzumaki Became Hokage";
