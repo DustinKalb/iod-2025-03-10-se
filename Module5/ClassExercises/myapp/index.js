8c9f87b38f317bb18e6579f155c6ae859fa52f5d
DustinKalb <dustinkalb9078@gmail.com>
2025-06-04 07:53:52 -0500

    made changes to class exercises

--- Module5/ClassExercises/myapp/index.js
+++ Module5/ClassExercises/myapp/index.js
@@ -3,11 +3,15 @@ const app = express();
 const port = 3000;
 const petRoutes = require("./routes/pet.routes");
 const calculatorRoutes = require("./routes/calculator.route");
+const userRoutes = require("./routes/user.routes");
+
+app.use(express.json());
 
 app.use("/", express.static("public"));
 
 app.use("/pet-api", petRoutes);
 app.use("/calculator", calculatorRoutes);
+app.use("/user", userRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening
--- Module5/ClassExercises/myapp/routes/calculator.route.js
+++ Module5/ClassExercises/myapp/routes/calculator.route.js
@@ -10,4 +10,14 @@ router.get("/add", (req, res) => {
   res.json({ result: sum });
 });
 
+router.post("/subtract", (req, res) => {
+  const body = req.body;
+  let number1 = parseInt(body.num1);
+  let number2 = parseInt(body.num2);
+  let sum = number1 + number2;
+  console.log(sum);
+  res.status(200);
+  res.json({ result: sum });
+});
+
 module.exports = router;
