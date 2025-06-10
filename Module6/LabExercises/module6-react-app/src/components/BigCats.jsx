cc4481570f891b28fdbb4b009703e434ea5cfaac
DustinKalb <dustinkalb9078@gmail.com>
2025-06-10 07:41:36 -0500

    finished lab 2

---
+++ Module6/LabExercises/module6-react-app/src/components/BigCats.jsx
@@ -0,0 +1,81 @@
+function BigCats() {
+  const cats = [
+    {
+      id: 1,
+      name: "Cheetah",
+      latinName: "Acinonyx jubatus",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Male_cheetah_facing_left_in_South_Africa.jpg/1200px-Male_cheetah_facing_left_in_South_Africa.jpg",
+    },
+    {
+      id: 2,
+      name: "Cougar",
+      latinName: "Puma concolor",
+      image:
+        "https://abilenezoo.org/wp-content/uploads/2023/05/Cougar-20221027-Caleb-8-scaled-1.jpg",
+    },
+    {
+      id: 3,
+      name: "Jaguar",
+      latinName: "Panthera onca",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/0/0a/Standing_jaguar.jpg",
+    },
+    {
+      id: 4,
+      name: "Leopard",
+      latinName: "Panthera pardus",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nagarhole_Kabini_Karnataka_India%2C_Leopard_September_2013.jpg/1200px-Nagarhole_Kabini_Karnataka_India%2C_Leopard_September_2013.jpg",
+    },
+    {
+      id: 5,
+      name: "Lion",
+      latinName: "Panthera leo",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg",
+    },
+    {
+      id: 6,
+      name: "Snow leopard",
+      latinName: "Panthera uncia",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Irbis4.JPG/1200px-Irbis4.JPG",
+    },
+    {
+      id: 7,
+      name: "Tiger",
+      latinName: "Panthera tigris",
+      image: "https://upload.wikimedia.org/wikipedia/commons/5/56/Tiger.50.jpg",
+    },
+  ];
+
+  const catItems = cats.map((cat) => (
+    <SingleCat
+      key={cat.id}
+      name={cat.name}
+      latinName={cat.latinName}
+      image={cat.image}
+    />
+  ));
+
+  return (
+    <div className="BigCats">
+      <ul style={{ listStyle: "none" }}>{catItems}</ul>
+    </div>
+  );
+}
+
+function SingleCat({ name, latinName, image }) {
+  return (
+    <li>
+      <h3 style={{ textDecoration: "underline" }}>{name}</h3>
+      <em>{latinName}</em>
+      <div>
+        <img src={image} alt={name} width={250} height={180} />
+      </div>
+    </li>
+  );
+}
+
+export default BigCats;
