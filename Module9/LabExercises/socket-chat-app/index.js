e85282ac4074aa5705bb6931716a712bcf449804
DustinKalb <dustinkalb9078@gmail.com>
2025-07-23 14:59:19 -0500

    finished lab 5

---
+++ Module9/LabExercises/socket-chat-app/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  // Broadcast to others that a user connected
+  socket.broadcast.emit("chat message", "A user connected");
+
+  // Listen for chat messages and broadcast to all
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+
+  // Broadcast to others that a user disconnected
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("chat message", "A user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
