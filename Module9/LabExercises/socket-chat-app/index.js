705df171f18f9fd787b6685b29c17d3e639d413f
DustinKalb <dustinkalb9078@gmail.com>
2025-08-11 20:47:48 -0500

    finished socket extension

--- Module9/LabExercises/socket-chat-app/index.js
+++ Module9/LabExercises/socket-chat-app/index.js
@@ -15,9 +15,12 @@ io.on("connection", (socket) => {
   socket.broadcast.emit("chat message", "A user connected");
 
   // Listen for chat messages and broadcast to all
-  socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
-    io.emit("chat message", msg);
+  socket.on("chat message", (data) => {
+    if (typeof data === "string") {
+      io.emit("chat message", data);
+    } else {
+      io.emit("chat message", { nickname: data.nickname, msg: data.msg });
+    }
   });
 
   // Broadcast to others that a user disconnected
